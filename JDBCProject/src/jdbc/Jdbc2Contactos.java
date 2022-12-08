d58d8ac86e16a3ba2223eefe94f2333ac9563ef8
Cristina Sanchez <c.sanchez.cadrecha@gmail.com>
2022-12-08 11:02:40 +0100

    [200~added ej1 and ej2 JDBC

---
+++ JDBCProject/src/jdbc/Jdbc2Contactos.java
@@ -0,0 +1,46 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Jdbc2Contactos {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		try {
+		
+		// Pasos para conectarme a una base de datos
+		// 1- Levantar el driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		//2- Establecer conexion usando la interface  (url, usuario, password)
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/capgemini_arg", "root", "admin");
+		
+		//3- Creamos la sentencia
+		Statement sql = con.createStatement();
+		
+		//4- Ejecutar consulta
+		//sql.execute(otroSql)
+		ResultSet rs = sql.executeQuery("select * from contactos");
+		
+		//5- Mostrar los resultados
+		while(rs.next()) {
+			System.out.println("Nombre: " + rs.getString("nombre"));
+			System.out.println("Email: " + rs.getString("email"));
+			System.out.println("--------------------------------------------");
+		}
+		//Cerrar la conexion
+		sql.close();
+		con.close();
+		
+	}catch(ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+	}
+}
+		
+
+	
+
+}
